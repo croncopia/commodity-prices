38b32fd765e42fccf5928dfb4851f3f94e854157
Reece Harris <me+git@reeceharris.net>
2026-06-18 17:28:22 +0100

    return all data

--- src/commodities/metals.ts
+++ src/commodities/metals.ts
@@ -5,18 +5,22 @@ import dailymetalprice from "../sources/dailymetalprice";
 import metalcharts from "../sources/metalcharts";
 
 export default async function () {
+  const goldapiio_data = await goldapiio();
+  // console.log(goldapiio_data)
+  const goldapicom_data = await goldapicom();
+  // console.log(goldapicom_data)
+  const metalpriceapi_data = await metalpriceapi();
+  // console.log(metalpriceapi_data)
+  const dailymetalprice_data = await dailymetalprice();
+  // console.log(dailymetalprice_data)
+  const metalcharts_data = await metalcharts();
+  // console.log(metalcharts_data)
 
-    // const goldapiio_data = await goldapiio()
-    // console.log(goldapiio_data)
-    // const goldapicom_data = await goldapicom()
-    // console.log(goldapicom_data)
-    // const metalpriceapi_data = await metalpriceapi()
-    // console.log(metalpriceapi_data)
-    // const dailymetalprice_data = await dailymetalprice()
-    // console.log(dailymetalprice_data)
-    const metalcharts_data = await metalcharts()
-    console.log(metalcharts_data)
-
-    return []
-
+  return [
+    goldapiio_data,
+    goldapicom_data,
+    metalpriceapi_data,
+    dailymetalprice_data,
+    metalcharts_data,
+  ];
 }
